cfaa3221de937319ad9069159f02009ce25af535
Sofía Schenone <sofiainesschenone@gmail.com>
2022-01-07 18:03:19 -0300

    Se sacaron las rutas de app.module y se pusieron en app-routing, también se agregaron algunos estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PracticesComponent } from './components/practices/practices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact-me', component: ContactMeComponent },
+  { path: 'practices',      component: PracticesComponent },
+  {
+    path: 'about-me', component: AboutMeComponent, data: { title: 'Heroes List' }
+  },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
