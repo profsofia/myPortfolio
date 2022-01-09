029bca49eb404e15373bbbfefb1c3e70bc79987e
Sofía Schenone <sofiainesschenone@gmail.com>
2022-01-09 09:56:40 -0300

    se corrigió la navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
 import { PracticesComponent } from './components/practices/practices.component';
 import { ContactMeComponent } from './components/contact-me/contact-me.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 const routes: Routes = [
   { path: 'contact-me', component: ContactMeComponent },
   { path: 'practices', component: PracticesComponent },
   { path: 'about-me', component: AboutMeComponent},
+  { path:'', redirectTo:'about-me', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
 
@@ -25,7 +27,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
